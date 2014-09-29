bb34465a239fdfa31de9481edfc53c0b5abd6c8e
gilbetliu(刘宏宇) <gilbetliu@tencent.com>
2014-09-29 15:32:38 +0800

    Release 0.1.3

--- src/main.js
+++ src/main.js
@@ -2,16 +2,84 @@
 
   // Create your component here
   // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  var optionTemplate = '<option value="{#value#}">{#team_name#}</option>';
+  var leagueUrl = 'http://sportswebapi.qq.com/tools/competition?random=';
+  var teamUrl = 'http://sportswebapi.qq.com/footballTeam/list?AppVersion=1.1&competitionId=';
+  function renderOptions (url) {
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      jsonp: 'callback',
+      method: 'get',
+    })
+    .done(function (data) {
+      console.log(data);
+    });
+  }
+
+  xtag.register('x-team-selector', {
     lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
+      created: function() {
+        console.log('created');
+        var self = this;
+        var ls = document.createElement('select');
+        ls.setAttribute('id', 'ts-league');
+        ls.innerHTML = '<option selected>loading...</option>';
+        self.appendChild(ls);
+        $.ajax({
+          url: leagueUrl,
+          dataType: 'jsonp',
+          jsonp: 'callback',
+          method: 'get',
+        })
+        .done(function (data) {
+          var options = '';
+          data[1].forEach(function (value) {
+            options = options.concat(
+              optionTemplate.replace('{#value#}', value.id)
+              .replace('{#team_name#}', value.shortName));
+          });
+          ls.innerHTML = options;
+        });
+      },
+      inserted: function() {
+        console.log('inserted');
+        // renderOptions('http://sportswebapi.qq.com/tools/competition?random=' + Math.random());
+      },
+      removed: function() {
+        console.log('removed');
+      },
+      attributeChanged: function() {
+        console.log('attributeChanged');
+      }
     }, 
-    events: { 
-    
+    events: {
+      change: function (e) {
+        var self = this;
+        console.log('change');
+        if (e.target.id === 'ts-league') {
+          var leagueId = e.target.value;
+          var ts = document.getElementById('ts-team') || document.createElement('select');
+          ts.setAttribute('id', 'ts-team');
+          ts.innerHTML = '<option selected>loading...</option>';
+          self.appendChild(ts);
+          $.ajax({
+            url: teamUrl + leagueId + '&random=' + Math.random(),
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            method: 'get',
+          })
+          .done(function (data) {
+            var options = '';
+            data[1].forEach(function (value) {
+              options = options.concat(
+                optionTemplate.replace('{#value#}', value.teamId)
+                .replace('{#team_name#}', value.name));
+            });
+            ts.innerHTML = options;
+          });
+        }
+      }
     },
     accessors: {
       
